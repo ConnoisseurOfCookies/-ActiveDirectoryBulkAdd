894758a3c17b2c938b56a8d31563aea1a7ce0b81
unknown <Administrator@KMAA.LAN>
2023-11-01 12:19:31 +1000

    Password Prompting Implemented
    Created a basic function that prompts the user whether they want to use a default password for each user or type them in individually

--- addAdUsers.ps1
+++ addAdUsers.ps1
@@ -85,6 +85,44 @@ foreach ($Group in $NewUserGroupList){
 $ADUserList = Get-ADUser -Filter "*" | Select-Object samaccountname
 
 
+# Prompt the user on whether they want to create passwords now or later
+$UsingDefaultPwd = $false
+$password
+
+# If user chooses default password, prompt twice and move on.
+# If user unique passwords, move on
+# Otherwise, prompt again
+do {
+    # The prompt
+    $UseDefaultPassword = Read-Host -Prompt "Do you want to use a default password for each user or assign a unique password for each individual user?`n (1) Use Default Password`n (2) Assign Individually`n"
+
+
+    switch ($UseDefaultPassword) {
+        1 { 
+            $passwordsMatch = $false
+            while(!$passwordsMatch){
+                $password = Read-Host -AsSecureString -Prompt "`nInsert Password"
+                $password2 = Read-Host -AsSecureString -Prompt "Type the Password Again"
+                $passwordsMatch = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)) -eq [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password2))
+                if ($passwordsMatch) {
+                    $UsingDefaultPwd = $true
+                    break
+                }
+                Write-Warning "`nPasswords do not match, please try again`n"
+            }
+            break
+        }
+
+        2 {
+            $UsingDefaultPwd = $false; Break
+        }
+        Default {Write-Warning "`nInvalid input, please try again`n" }
+    }
+} while (
+    # Do while neither 1 or 2 is selected
+    !($UseDefaultPassword -eq 1 -or $UseDefaultPassword -eq 2)
+)
+
 # Iterate through each user in users
 ForEach ($user in $users) {
     
@@ -99,11 +137,28 @@ ForEach ($user in $users) {
     $grp2 = $user.Grp2 
     $grp3 = $user.Grp3
     $Role = $user.Role
-    $password = ConvertTo-SecureString $user.Password -AsPlainText -Force
+
+    
+    
 
     if($ADUserList.samaccountname.Contains($username)){
         Write-Output "User with accountname $username is already added to the domain. Check user details."
     }else {
+        # Check whether or not user chooses default passwords
+        if (!$UsingDefaultPwd) {
+
+            # Prompt for user password
+            $passwordsMatch = $false
+            do {
+                $password = Read-Host -AsSecureString -Prompt "Create password for $username"
+                $password2 = Read-Host -AsSecureString -Prompt "Type the password again"
+                $passwordsMatch = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)) -eq [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password2))
+                if(!$passwordsMatch){
+                    Write-Warning "Passwords do not match, please try again:"
+                }
+            } while (!$passwordsMatch)
+        }
+
         # Create new AD user
         New-ADUser  -GivenName $firstname -Surname $lastname -Name "$firstname $lastname" -UserPrincipalName $username -SamAccountName $username -Path $ouFullAddress -AccountPassword $password -ChangePasswordAtLogon $False -PasswordNeverExpires $True -EmailAddress $email -Department $Role -Enabled $True
         
